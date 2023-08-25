2eb84b1bea8bbee5e036a5bbb82f10acf5bb670c
Orun Bhuiyan <orun@agencyundone.com>
2023-08-25 17:31:46 -0400

    feat: add CID output

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+
+	cid "github.com/ipfs/go-cid"
+	multihash "github.com/multiformats/go-multihash"
 )
 
 func main() {
@@ -20,9 +23,34 @@ func main() {
 		return
 	}
 
-	// Print files and directories
+	// Print files and their CIDs
 	for _, file := range files {
 		fmt.Printf("%s\t", file.Name())
+		if !file.IsDir() { // Only process files, not directories
+			cidStr, err := computeCID(file.Name())
+			if err != nil {
+				fmt.Printf("ERROR: %s\n", err)
+			} else {
+				fmt.Printf("%s\n", cidStr)
+			}
+		} else {
+			// For directories, just print the name without a CID
+			fmt.Println()
+		}
 	}
-	fmt.Println()
+}
+
+func computeCID(filename string) (string, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	hash, err := multihash.Sum(content, multihash.SHA2_256, -1)
+	if err != nil {
+		return "", err
+	}
+
+	c := cid.NewCidV1(cid.Raw, hash)
+	return c.String(), nil
 }
